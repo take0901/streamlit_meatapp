d12da48fe76f77fbb7c2b8c958f70456b6a6454a
username <tkenshi19@gmail.com>
2023-08-13 00:11:27 +0900

    cv2の処理を消した

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import av
 from streamlit_webrtc import webrtc_streamer
 from PIL import Image
-import cv2
 from ultralytics import YOLO
 
 model = YOLO('static/meat.pt')
@@ -11,10 +10,8 @@ def video_frame_callback(frame):
     img = Image.fromarray(frame)
     img = model.predict(img)
     img_array = img[0].plot()
-    #predict後の画像は色がおかしいので直す
-    img_array_color = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
 
-    return av.VideoFrame.from_ndarray(img_array_color, format="bgr24")
+    return av.VideoFrame.from_ndarray(img_array, format="bgr24")
 
 
 ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback,
